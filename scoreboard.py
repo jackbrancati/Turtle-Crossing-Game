6e833f4ae41f481488498cb1cbc75d90464de864
jackbrancati <162787047+jackbrancati@users.noreply.github.com>
2024-06-19 16:29:28 -0400

    Add files via upload

---
+++ scoreboard.py
@@ -0,0 +1,25 @@
+FONT = ("Courier", 24, "normal")
+from turtle import Turtle
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.level = 1
+        self.color('black')
+        self.penup()
+        self.level = 1
+        self.goto(-280,250)
+        self.update_scoreboard()
+        self.hideturtle()
+
+    def update_scoreboard(self):
+        self.write(f"Level: {self.level}", align="left", font=FONT)
+
+    def increase_level(self):
+        self.level += 1
+        self.clear()
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write(f"GAME OVER", align="center",font=FONT)
